314b2b664ce05823a42fe18218154886b2f8bbbd
hoanghuy <hoanghuybaby2002@gmail.com>
2024-12-05 02:31:59 +0700

    fix error

--- robot_path_controller/scripts/main.py
+++ robot_path_controller/scripts/main.py
@@ -17,7 +17,7 @@ class Dijkstra():
                 self.Grid_Planning_Information[(X,Y)]={"Status":"Finding","Point":10000000,"From":()}
                 self.Grid_List.append((X,Y))
 
-    def Reset_Path_Planning(self):
+    def __Reset_Path_Planning(self):
         for Grid in self.Grid_Planning_Information:
             self.Grid_Planning_Information[Grid]["Status"] = "Finding" 
             self.Grid_Planning_Information[Grid]["Point"]  = 10000000
@@ -78,6 +78,7 @@ class Dijkstra():
 
     
     def Find_Path(self,Start_Grid:tuple,End_Grid:tuple,Penalty_Map:dict):
+        self.__Reset_Path_Planning()
         self.__Update_Status_Of_Grid_Planning(Grid=Start_Grid,Status="Start")
         self.__Update_Point_Of_Grid_Planning(Grid=Start_Grid,Point=0)
         Grid_State_End = Start_Grid
@@ -239,7 +240,6 @@ class Controller():
         Length_Axis = self.Penalty_Map.Get_Number_X_Axis_In_Map()
         Penalty_Map = self.Penalty_Map.Get_Penalty_Map()
         Position = self.Robot_Position.Get_Position_At_Now()
-        New_Pose = ()
         X_Now = Position[0]
         Y_Now = Position[1]
         New_Pose = ()
@@ -280,9 +280,8 @@ class Controller():
     def Determine_New_Position_For_Robot(self):
         Positon_Now = self.Robot_Position.Get_Position_At_Now()
         Penalty_Map_Now = self.Penalty_Map.Get_Penalty_Map()
-        self.Path_Planning.Reset_Path_Planning()
         Target_Pose = self.__Determine_Possible_NewPosition_To_Move()
-        print(Target_Pose)
+        print(f"New Target Position is {Target_Pose}")
         Path = self.Path_Planning.Find_Path(Start_Grid=Positon_Now,End_Grid=Target_Pose,Penalty_Map=Penalty_Map_Now)
         return Path[0]
     
