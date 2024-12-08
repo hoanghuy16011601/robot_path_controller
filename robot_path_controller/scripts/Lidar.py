62a3f822962d504f1375966a71d7510917479f1c
hoanghuy <hoanghuybaby2002@gmail.com>
2024-12-08 22:58:21 +0700

    add file lidar.py

--- robot_path_controller/scripts/Lidar.py
+++ robot_path_controller/scripts/Lidar.py
@@ -2,17 +2,13 @@
 
 import rospy
 from sensor_msgs.msg import LaserScan
+import time
 
 def scan_callback(msg):
-    # In thông tin cơ bản về quét laser
-    rospy.loginfo("Số tia quét: %d", len(msg.ranges))
-    rospy.loginfo("Góc quét: từ %.2f đến %.2f radian", msg.angle_min, msg.angle_max)
-    rospy.loginfo("Góc giữa các tia: %.5f radian", msg.angle_increment)
-    
     # Lấy giá trị khoảng cách của tia giữa
     mid_index = len(msg.ranges) // 2
     rospy.loginfo("Khoảng cách tia giữa: %.2f mét", msg.ranges[mid_index])
-    
+    time.sleep(0.5)
     # Lấy toàn bộ giá trị khoảng cách (nếu cần)
     ranges = msg.ranges
 
