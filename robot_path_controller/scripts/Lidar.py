fc0a2011d35525b1d3b6e211e3b55b8e9fdb767e
hoanghuy <hoanghuybaby2002@gmail.com>
2024-12-08 23:24:15 +0700

    add file lidar.py

--- robot_path_controller/scripts/Lidar.py
+++ robot_path_controller/scripts/Lidar.py
@@ -4,12 +4,30 @@ import rospy
 from sensor_msgs.msg import LaserScan
 import time
 
+class Lidar():
+    def __init__(self):
+        self.Head_Distance = 1000
+        self.Back_Distance = 1000
+        self.Left_Distance = 1000
+        self.Right_Distance = 1000
+
+    def Get_Distance(self):
+        return (self.Head_Distance ,self.Back_Distance ,self.Left_Distance ,self.Right_Distance)
+    
+    def Convert_To_Algorithm_Distance(Ranges):
+        print("test")
+        # Get Head
 def scan_callback(msg):
     # Lấy giá trị khoảng cách của tia giữa
-    mid_index = len(msg.ranges) // 2
-    rospy.loginfo("Khoảng cách tia giữa: %.2f mét", msg.ranges[mid_index])
+    head_index = len(msg.ranges) // 2
+    left_index = len(msg.ranges) // 4
+    right_index = len(msg.range)*3 //4
+    back_index = 0
+    rospy.loginfo("Head_Distance: %.2f m", msg.ranges[head_index])
+    rospy.loginfo("Left_Distance: %.2f m", msg.ranges[left_index])
+    rospy.loginfo("Back_Distance: %.2f m", msg.ranges[back_index])
+    rospy.loginfo("Right_Distance: %.2f m", msg.ranges[right_index])
     # Lấy toàn bộ giá trị khoảng cách (nếu cần)
-    ranges = msg.ranges
 
 def main():
     # Khởi tạo ROS node
