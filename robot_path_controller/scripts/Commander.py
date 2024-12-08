335af7c594df28e114dfac527d9c21c11f9bb860
hoanghuy <hoanghuybaby2002@gmail.com>
2024-12-08 12:04:42 +0700

    clean message call back handler

--- robot_path_controller/scripts/Commander.py
+++ robot_path_controller/scripts/Commander.py
@@ -309,10 +309,10 @@ class Controller():
                         New_Pose = (X_Check,Y_Check)
                         Penalty_Point_Of_New_Pose = Penalty_Map_For_Possible_Pose[X_Check][Y_Check]
                     elif Penalty_Map_For_Possible_Pose[X_Check][Y_Check] == Penalty_Point_Of_New_Pose:
-                        if (X_Check < New_Pose[0]):
+                        if (X_Check < New_Pose[0] and X_Check > 12):
                             New_Pose = (X_Check,Y_Check)
                         elif (X_Check == New_Pose[0]):
-                            if (Y_Check < New_Pose[1]):
+                            if (Y_Check < New_Pose[1] and Y_Check > 12):
                                 New_Pose = (X_Check,Y_Check)
                             else:
                                 pass
